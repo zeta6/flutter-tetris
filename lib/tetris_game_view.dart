4d47a46d2f63759565f8915f1dcd351ba76ff9f6
Ohjin <ohjin13444@gmail.com>
2024-05-07 07:46:41 +0900

    feat: Change block rendering to refer to game board objects

--- lib/tetris_game_view.dart
+++ lib/tetris_game_view.dart
@@ -14,8 +14,7 @@ class _TetrisGameViewState extends State<TetrisGameView> {
       TetrominoType.values.map((e) => Tetromino(type: e)).toList()..shuffle();
   late Tetromino? activeTetromino = tetrominoBag.first;
   late Tetromino? nextTetromino = tetrominoBag[1];
-  List<List<int>> gameBoard =
-      List.generate(20, (i) => List.generate(10, (j) => 0));
+
   int currentBlockX = 2;
   int currentBlockY = 0;
 
@@ -42,6 +41,9 @@ class _TetrisGameViewState extends State<TetrisGameView> {
     currentBlockY = 0;
   }
 
+  List<List<int>> gameBoard =
+      List.generate(20, (columnIndex) => List.generate(10, (rowIndex) => 0));
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,59 +58,104 @@ class _TetrisGameViewState extends State<TetrisGameView> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Container(
-                  width: 200,
-                  height: 400,
+                  width: 210,
+                  height: 420,
                   color: Colors.grey,
                   child: Stack(
                     children: [
-                      // 게임 보드 그리기
-                      GridView.builder(
-                        physics: NeverScrollableScrollPhysics(),
-                        itemCount: 200,
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 10,
-                        ),
-                        itemBuilder: (BuildContext context, int index) {
-                          int row = index ~/ 10;
-                          int col = index % 10;
-                          return Container(
-                            color: gameBoard[row][col] == 0
-                                ? Colors.white
-                                : Colors.blue,
-                            margin: EdgeInsets.all(1),
-                          );
-                        },
+                      Column(
+                        children: [
+                          for (List<int> rowList in gameBoard)
+                            Row(
+                              children: [
+                                for (int row in rowList)
+                                  Container(
+                                    width: 19,
+                                    height: 19,
+                                    color:
+                                        row == 0 ? Colors.white : Colors.blue,
+                                    margin: const EdgeInsets.all(1),
+                                  ),
+                              ],
+                            ),
+                        ],
                       ),
+
+                      // GridView.builder(
+                      //   physics: NeverScrollableScrollPhysics(),
+                      //   itemCount: 200,
+                      //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      //     crossAxisCount: 10,
+                      //   ),
+                      //   itemBuilder: (BuildContext context, int index) {
+                      //     int row = index ~/ 10;
+                      //     int col = index % 10;
+                      //     return Container(
+                      //       color: gameBoard[row][col] == 0
+                      //           ? Colors.white
+                      //           : Colors.blue,
+                      //       margin: EdgeInsets.all(1),
+                      //     );
+                      //   },
+                      // ),
                       // 현재 블록 그리기
                       if (activeTetromino != null)
                         Positioned(
-                          top: currentBlockY * 20.0,
-                          left: currentBlockX * 20.0,
-                          child: Container(
-                            width: 80,
-                            height: 80,
-                            child: GridView.builder(
-                              physics: NeverScrollableScrollPhysics(),
-                              itemCount: 16,
-                              gridDelegate:
-                                  SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 4,
-                              ),
-                              itemBuilder: (BuildContext context, int index) {
-                                int row = index ~/ 4;
-                                int col = index % 4;
-                                return Container(
-                                  color: activeTetromino!
-                                              .currentShape.shape[row][col] ==
-                                          0
-                                      ? Colors.transparent
-                                      : activeTetromino!.type.color,
-                                  margin: EdgeInsets.all(1),
-                                );
-                              },
+                          top: currentBlockY * 21,
+                          left: currentBlockX * 21,
+                          // top: 0,
+                          // left: 0,
+                          child: SizedBox(
+                            child: Column(
+                              children: [
+                                for (List<int> rowList
+                                    in activeTetromino!.currentShape.shape)
+                                  Row(
+                                    children: [
+                                      for (int row in rowList)
+                                        Container(
+                                          width: 19,
+                                          height: 19,
+                                          color: row == 0
+                                              ? Colors.white
+                                              : activeTetromino!.type.color,
+                                          margin: const EdgeInsets.all(1),
+                                        ),
+                                    ],
+                                  ),
+                              ],
                             ),
                           ),
                         ),
+
+                      // Positioned(
+                      //   top: currentBlockY * 20.0,
+                      //   left: currentBlockX * 20.0,
+                      //   child: Container(
+                      //     width: 80,
+                      //     height: 80,
+                      //     child: GridView.builder(
+                      //       physics: NeverScrollableScrollPhysics(),
+                      //       itemCount: 16,
+                      //       gridDelegate:
+                      //           SliverGridDelegateWithFixedCrossAxisCount(
+                      //         crossAxisCount: 4,
+                      //       ),
+                      //       itemBuilder: (BuildContext context, int index) {
+                      //         int row = index ~/ 4;
+                      //         int col = index % 4;
+                      //         return Container(
+                      //           color: activeTetromino!.currentShape.shape[row]
+                      //                       [col] ==
+                      //                   0
+                      //               ? Colors.transparent
+                      //               : activeTetromino!.type.color,
+                      //           margin: EdgeInsets.all(1),
+                      //         );
+                      //       },
+                      //     ),
+                      //   ),
+                      // ),
                     ],
                   ),
                 ),
